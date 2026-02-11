a5d151bb1bc0b0b7e33d03abfe9de0b385693533
Cimarron Wortham <worthamc@gmail.com>
2026-02-11 15:29:46 -0800

    Update SchematicScaleDiagram.m

--- SchematicScaleDiagram.m
+++ SchematicScaleDiagram.m
@@ -8,13 +8,16 @@ basedir = '/Volumes/SanDiskExtremePro/research/JeffreyEarly/FineScaleData/';
 % basedir = '/Users/jearly/Dropbox/FineScaleData/';
 
 % filename = '7cms/fine-scale-hydrostatic-50km-128-222-cyclone-7cms-cyclogeostrophic.nc';
-% runname = '7cm/s, 50km, 128^2 x 222 cyclone';
+% shortname = '7cm/s cyclone';
+
 % filename = '7cms/fine-scale-hydrostatic-50km-128-222-anticyclone-7cms-cyclogeostrophic.nc';
-% runname = '7cm/s, 50km, 128^2 x 222 anticyclone';
-filename = '25cms/fine-scale-hydrostatic-50km-256-443-anticyclone-cyclogeostrophic.nc';
-runname = '25cm/s, 50km, 256^2 x 222 anticyclone';
-% filename = 'fine-scale-hydrostatic-50km-256-443-one-half-dealias.nc';
-% runname = '50km, 256^2 x 443 anticyclone';
+% shortname = '7cm/s anticyclone';
+
+% filename = '25cms/fine-scale-hydrostatic-50km-256-443-anticyclone-cyclogeostrophic.nc';
+% shortname = '25cm/s anticyclone';
+
+filename = 'fine-scale-hydrostatic-50km-256-443-one-half-dealias.nc';
+shortname = 'GM IGW field';
 
 % load the file
 wvd = WVDiagnostics(fullfile(basedir,filename));
@@ -39,8 +42,8 @@ C = orderedcolors("gem");
 colorDictionary = dictionary("vertical_scalar_diffusivity",{C(1,:)});
 colorDictionary{"adaptive_damping"} = C(2,:);
 
-timeIndices = (length(t)-4*4):length(t);
-% timeIndices = length(t);
+% timeIndices = (length(t)-4*4):length(t);
+timeIndices = length(t);
 
 % load spectral energy fluxes
 energy_fluxes = wvd.exactEnergyFluxesTemporalAverage(timeIndices=timeIndices);
@@ -91,31 +94,129 @@ te_diss_tot = int_vol( te_diss );
 % Ozmidov scale, m_O in Kunze, JPO, 2019
 mOzmidov = sqrt( permute(wvt.N2.^(3/2), [2 3 1]) ./ te_diss );
 LOzmidov = 2*pi./mOzmidov;
+% LOzmidov = 2*pi*sqrt( te_diss./permute(wvt.N2.^(3/2), [2 3 1]) );
 
 % rolloff wavenumber, m_c or m_f in Kunze, JPO, 2019
 mCoriolis = sqrt( permute(wvt.f*wvt.N2, [2 3 1]) ./ te_diss );
 LCoriolis = 2*pi./mCoriolis;
+% LCoriolis = 2*pi*sqrt( te_diss./permute(wvt.f*wvt.N2, [2 3 1]) );
 
 % horizontal Coriolis wavenumber
 kCoriolis = sqrt( wvt.f^3 ./ te_diss );
 LhCoriolis = 2*pi./kCoriolis;
+% LhCoriolis = 2*pi*sqrt( te_diss./wvt.f^3 );
 
 fprintf("Maximum Ozmidov length scale %0.1f m.\n", max(LOzmidov(:)))
 fprintf("Maximum vertical rolloff length scale %0.1f m.\n", max(LCoriolis(:)))
 fprintf("Maximum horizontal rolloff length scale %0.1f m.\n", max(LhCoriolis(:)))
 
+%% make figures
 
-%% make figure
+runname = sprintf("%s, $%d\\mathrm{km}^2$ x $%d\\mathrm{m}$, $%d^2$ x $%d$", shortname, wvt.Lx/1000, wvt.Lz, wvt.Nx, wvt.Nz);
+
+%% histogram
+
+fig = figure('Units', 'points', 'Position', [50 50 600 300]);
+tl = tiledlayout(fig,2,3,"TileSpacing","compact","Padding","compact");
+
+% line colors
+C = orderedcolors("gem"); 
+colorDictionary = dictionary("LO",{C(1,:)});
+colorDictionary{"Lzf"} = C(2,:);
+colorDictionary{"Lhf"} = C(3,:);
+
+% histogram options
+normalization = 'probability';
+% normalization = 'cdf';
+
+% peakOp = @(x) mean(x); peakOpName = 'mean value';
+peakOp = @(x) median(x); peakOpName = 'median value';
+
+% averaging indices
+domainFrac = 1/4;
+xInd = (floor(wvt.Nx/2)-floor(wvt.Nx*domainFrac/2)):(floor(wvt.Nx/2)+floor(wvt.Nx*domainFrac/2));
+yInd = xInd;
+zInd = 1:wvt.Nz;
+
+tile = nexttile(tl,1,[2,1]);
+hold on
+LOzmidovProfile = squeeze( mean(mean(LOzmidov(xInd,yInd,zInd),2),1) );
+LCoriolisProfile = squeeze( mean(mean(LCoriolis(xInd,yInd,zInd),2),1) );
+LhCoriolisProfile = squeeze( mean(mean(LhCoriolis(xInd,yInd,zInd),2),1) );
+plot(LOzmidovProfile,wvt.z,'Color',colorDictionary{'LO'},'linewidth',2,'DisplayName','L_{O}')
+plot(LCoriolisProfile,wvt.z,'Color',colorDictionary{'Lzf'},'linewidth',2,'DisplayName','Lz_{f}')
+plot(LhCoriolisProfile,wvt.z,'Color',colorDictionary{'Lhf'},'linewidth',2,'DisplayName','Lh_{f}')
+% add grid spacing
+plot(diff(wvt.z),wvt.z(1:end-1),'k--')
+% fine tune plot
+xlog
+set(gca, 'XMinorTick', 'on')
+xticks(logspace(-1, 3, 5));
+xlabel('length scale (m)')
+ylabel('depth (m)')
+box on
+% legend('Location','best')
+
+
+tile = nexttile(tl,2,[2,2]);
+hold on
+histLO = histogram(LOzmidov(:),'FaceColor',colorDictionary{'LO'},'EdgeColor','none','Normalization',normalization,'DisplayName','Ozmidov length');
+histLzf = histogram(LCoriolis(:),'FaceColor',colorDictionary{'Lzf'},'EdgeColor','none','Normalization',normalization,'DisplayName','Vertical Coriolis length');
+histLhf = histogram(LhCoriolis(:),'FaceColor',colorDictionary{'Lhf'},'EdgeColor','none','Normalization',normalization,'DisplayName','Horizontal Coriolis length');
+xlog
+xlim([0,1000])
+xlabel('length scale (m)')
+ylabel(normalization)
+% yticklabels([])
+box on
+legend('Location','best')
+% label peaks
+% LO
+% [maxCount, maxIdx] = max(histLO.Values);
+% plot(histLO.BinEdges(maxIdx),maxCount,'k.','HandleVisibility','off')
+% text(histLO.BinEdges(maxIdx),maxCount+3e-4,sprintf('%0.1f m',histLO.BinEdges(maxIdx)))
+[~,idx] = min(abs(histLO.BinEdges-peakOp(LOzmidov(:))));
+plot(peakOp(LOzmidov(:)),histLO.Values(idx),'k.','DisplayName',peakOpName)
+text(peakOp(LOzmidov(:)),histLO.Values(idx)+3e-4,sprintf('%0.0f m',peakOp(LOzmidov(:))))
+% Lzf
+% [maxCount, maxIdx] = max(histLzf.Values);
+% plot(histLzf.BinEdges(maxIdx),maxCount,'k.','HandleVisibility','off')
+% text(histLzf.BinEdges(maxIdx),maxCount+3e-4,sprintf('%0.1f m',histLzf.BinEdges(maxIdx)))
+[~,idx] = min(abs(histLzf.BinEdges-peakOp(LCoriolis(:))));
+plot(peakOp(LCoriolis(:)),histLzf.Values(idx),'k.','HandleVisibility','off')
+text(peakOp(LCoriolis(:)),histLzf.Values(idx)+3e-4,sprintf('%0.0f m',peakOp(LCoriolis(:))))
+% Lhf
+% [maxCount, maxIdx] = max(histLhf.Values);
+% plot(histLhf.BinEdges(maxIdx),maxCount,'k.','HandleVisibility','off')
+% text(histLhf.BinEdges(maxIdx),maxCount+3e-4,sprintf('%0.1f m',histLhf.BinEdges(maxIdx)))
+[~,idx] = min(abs(histLhf.BinEdges-peakOp(LhCoriolis(:))));
+plot(peakOp(LhCoriolis(:)),histLhf.Values(idx),'k.','HandleVisibility','off')
+text(peakOp(LhCoriolis(:)),histLhf.Values(idx)+3e-4,sprintf('%0.0f m',peakOp(LhCoriolis(:))))
+
+% model grid spacing
+xline(min(diff(wvt.z)),'k--','DisplayName','min. \Deltaz')
+xline(wvt.Lx/wvt.Nx,'k:','DisplayName','\Deltax')
+
+% title
+title(tl,runname,'Interpreter','latex')
+
+% save
+if shouldExportFigures
+    exportgraphics(fig,fullfile(figureFolder,'TurbulenceScales.png'))
+end
+
+
+%% energy flux
 
 % plot
 fig = figure('Units', 'points', 'Position', [50 50 400 400]);
 set(gcf,'PaperPositionMode','auto')
 set(gcf, 'Color', 'w');
-fig = wvd.plotPoissonFlowOverContours(figureHandle=fig,vectorDensityLinearTransitionWavenumber=10^(-3.9),quiverScale=3,jmax=2e-3,kmax=2e-3,inertialFlux=flux_advective,addFrequencyContours=true,addKEPEContours=false,yAxisLabel="deformation length");
-% fig = wvd.plotPoissonFlowOverContours(figureHandle=fig,vectorDensityLinearTransitionWavenumber=10^(-3.9),quiverScale=3,jmax=2e-3,kmax=2e-3,inertialFlux=flux_advective,addFrequencyContours=true,addKEPEContours=false,yAxisLabel="vertical mode");
+% fig = wvd.plotPoissonFlowOverContours(figureHandle=fig,vectorDensityLinearTransitionWavenumber=10^(-3.9),quiverScale=3,jmax=2e-3,kmax=2e-3,inertialFlux=flux_advective,addFrequencyContours=true,addKEPEContours=false,yAxisLabel="deformation length");
+fig = wvd.plotPoissonFlowOverContours(figureHandle=fig,vectorDensityLinearTransitionWavenumber=10^(-3.9),quiverScale=3,jmax=2e-3,kmax=2e-3,inertialFlux=flux_advective,addFrequencyContours=true,addKEPEContours=false,yAxisLabel="vertical mode");
 ax = gca;
 box on
-title(runname,'Interpreter','none')
+title(runname,'Interpreter','latex')
 
 % save
 if shouldExportFigures
